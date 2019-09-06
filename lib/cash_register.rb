a78ce7fc5c8f20f998698a94daffc585ae4e8d57
Brad Alexander <brad.alexander913@gmail.com>
2019-09-06 01:01:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,15 @@
 class CashRegister 
   
+  attr_accessor :emp_discount, :total
+  
   def initialize(emp_discount=0)
     @total = 0
     @emp_discount = emp_discount
   end
   
+  def discount 
+    @emp_discount 
+  end
   
   
 end
